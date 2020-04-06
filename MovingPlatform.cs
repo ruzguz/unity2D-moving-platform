c62367b5c2d0e1daff0c173b02fe4c8f802c0e32
Rusben Guzman <rusbenguzman@gmail.com>
2020-04-06 11:51:02 -0400

    MovementType enum created, movementType var created to know how the platform is gonna move

--- MovingPlatform.cs
+++ MovingPlatform.cs
@@ -2,8 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum MovementType {
+    line,
+    circle ,
+    zigzag
+};
+
 public class MovingPlatform : MonoBehaviour
 {
+
+    // General vars
+    [SerializeField]
+    MovementType movementType;
+
     // Start is called before the first frame update
     void Start()
     {
