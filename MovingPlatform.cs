82f7f3ac38ed8e11653ef0e3d65e36d49c79bf74
Rusben Guzman <rusbenguzman@gmail.com>
2020-04-06 12:12:47 -0400

    some comments added, MovementOrientation enum created, movementOrientation var created, lineMovementDistance var created

--- MovingPlatform.cs
+++ MovingPlatform.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
+// Platform Movement Type: show the platform is going ti move 
 public enum MovementType {
     line,
     circle ,
     zigzag
 };
+
+// Movment orientation (if is applicable)
+public enum MovementOrientation {
+    horizontal,
+    vertical
+}
 
 public class MovingPlatform : MonoBehaviour
 {
@@ -15,6 +23,12 @@ public class MovingPlatform : MonoBehaviour
     // General vars
     [SerializeField]
     MovementType movementType;
+    [SerializeField]
+    MovementOrientation movementOrientation;
+    public float speed = 5f;
+
+    // Line movement vars
+    public float lineMovementDistance = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,4 +41,5 @@ public class MovingPlatform : MonoBehaviour
     {
         
     }
+
 }
